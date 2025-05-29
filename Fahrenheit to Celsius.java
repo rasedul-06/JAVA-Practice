d14dd8f487179813cc9d621b0d40bbd78f41d99f
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-05-29 22:00:07 +0600

    Create Fahrenheit to Celsius.java

---
+++ Fahrenheit to Celsius.java
@@ -0,0 +1,20 @@
+package temperature;
+// Write a program that converts temperature from Fahrenheit to Celsius
+
+import java.util.Scanner;
+
+public class Fahrenheit_to_Celsius {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double farn, cels;
+
+        System.out.print("Enter the value in Fahrenheit: ");
+        farn = input.nextDouble();
+
+        cels = (farn - 32) * 5 / 9;
+
+        System.out.println("The Celsius value is: " + cels);
+    }
+}
