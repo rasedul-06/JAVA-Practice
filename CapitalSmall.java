a12b1ac0ce27e0ad08e443c1c3f39d9370eca959
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-05-31 21:11:09 +0600

    Update CapitalSmall.java

--- CapitalSmall.java
+++ CapitalSmall.java
@@ -1,4 +1,4 @@
-package Relational;
+package Logical;
 //Write a program for Capital letter and Small letter
 import java.util.Scanner;
 
@@ -8,12 +8,22 @@ public class CapitalSmall{
         System.out.print("Enter any Character: ");
         char ch = input.next().charAt(0);
 
-        if(Character.isUpperCase(ch)){
+        // using logical operator
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Small Letter.");
+        }else if(ch >= 'A' && ch <= 'Z'){
+            System.out.println("Capital Letter.");
+        }else{
+            System.out.println("Not a valid character.");
+        }
+
+        // using method
+        /*if(Character.isUpperCase(ch)){
             System.out.println("Capital Letter");
         }else if(Character.isLowerCase(ch)){
             System.out.println("Small Letter");
         }else {
             System.out.println("It's not a Letter");
-        }
+        }*/
     }
 }
