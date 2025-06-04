da818968df5b94677e505f86e7d377cad89c46ec
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-06-04 20:04:29 +0600

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,20 @@
+package Loop;
+//build a program by For Loop.
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++){
+            System.out.println(i+" Bangladesh"); //print Bangladesh 10 times with serial 
+        }
+        for(int j = 1; j <=50; j++){
+            System.out.println(j); //print 1-50
+        }
+	for(int k=2; k<=100; k=k+2){
+	    System.out.println(k); //print even numbers between 1-100
+	}
+	for(int x=10; x>=1; x--){
+	    System.out.println(x) //print 1-10 reversly 
+	}
+    }
+}
