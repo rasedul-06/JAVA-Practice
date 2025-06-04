1bc3584a01957d2533b7cb8028c5c2f9c1188761
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-06-04 20:07:05 +0600

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,31 @@
+package Loop;
+//build a program by Do-While Loop.
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int i=1;
+        do{
+            System.out.println(i+" Bangladesh");
+            i++;
+        }while(i<=10);
+
+        int j=1;
+        do{
+            System.out.println(j);
+            j++;
+        }while(j<=50);
+
+        int k=1;
+        do{
+            System.out.println(k);
+            k=k+2;
+        }while(k<=100);
+
+        int x=10;
+        do{
+            System.out.println(x);
+            x--;
+        }while(x>=1);
+    }
+}
