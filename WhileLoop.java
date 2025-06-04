ee087a55807495bf693e2922cf3772420330d374
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-06-04 20:05:59 +0600

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,31 @@
+package Loop;
+//build a program by While Loop.
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i=1;
+        while(i<=10){
+            System.out.println(i+" Bangladesh"); //print Bangladesh 10 times with serial
+            i++;
+        }
+
+        int j=1;
+        while(j<=50){
+            System.out.println(j); //print 1-50
+            j++;
+        }
+
+        int k=2;
+        while(k<=100){
+            System.out.println(k); //print even numbers between 1-100
+            k=k+2;
+        }
+
+        int x=10;
+        while(x>=1){
+            System.out.println(x); //print 1-10 reversly
+            x--;
+        }
+    }
+}
