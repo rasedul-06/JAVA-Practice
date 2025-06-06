4901d360ad3ef7204f1f4a827ce506e37ed9f762
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-06-06 23:53:25 +0600

    Create AdvancePrimeNumber.java

---
+++ AdvancePrimeNumber.java
@@ -0,0 +1,28 @@
+package Loop;
+//Example of prime number
+import java.util.Scanner;
+public class AdvancePrimeNumber{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num,i,count = 0;
+        
+        System.out.print("Enter any Integer number: ");
+        num = input.nextInt();
+        System.out.println();
+        if(num==0 || num==1){
+            System.out.println("It's not a prime number.");
+        }else {
+            for (i = 2; i < num; i++) {
+                if (num % 2 == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println("Prime number.");
+            } else {
+                System.out.println("Not a prime number");
+            }
+        }
+    }
+}
